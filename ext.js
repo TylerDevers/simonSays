a4679b837a4f5e77585b5a8c81520a09aec638de
TylerDevers <devers.tyler@gmail.com>
2017-09-12 12:04:53 -0700

    playing with colorChange()

--- ext.js
+++ ext.js
@@ -1,1 +1,12 @@
-
+/* psuedo code
+ * colorChange {
+ * 		onclick class changes;
+ * 		class reverts to original setting;
+ * }
+ * 
+ */
+ 
+function colorChange(element) {
+	console.log("clicked");
+	element.setAttribute("id", "lightTest");
+}
