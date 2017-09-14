58f9924101d2d70c5c10c24c82f0fc369a94a73b
TylerDevers <devers.tyler@gmail.com>
2017-09-14 10:03:59 -0700

    added simonSays object

--- ext.js
+++ ext.js
@@ -4,9 +4,33 @@
  * 		class reverts to original setting;
  * }
  * 
+ * use objects/methods to use dot notation
+ * 
  */
  
-function colorChange(element) {
-	console.log("clicked");
-	element.setAttribute("id", "lightTest");
+var yellow = "yellow";
+var red = "red";
+var blue = "blue";
+var green = "green";
+var lightYellow = "#FBFB98";
+var lightRed = "#FB9595";
+var lightBlue = "#98CCFF";
+var lightGreen = "#84FA84";
+
+var simonSays = {
+		color: function(color) {
+			console.log(color);
+		}
+} 
+ 
+function redChange(element) {
+	var color = simonSays.color(element.id);
+	console.log("clicked " + color + tile);
+	element.style.background = lightRed;
+	
+	//element.setAttribute("background", "#FF6161");
 }
+
+/*
+ * ideas:
+ * 
