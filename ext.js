5eb3941e36a64c16035362c14ad20b2e47d467f1
TylerDevers <devers.tyler@gmail.com>
2017-09-15 11:53:51 -0700

    nextColor() and userColor() are working. Use index to match arrays

--- ext.js
+++ ext.js
@@ -9,30 +9,36 @@ var index = 0;
  */
 function nextColor() {
 	var newColor = colors[Math.floor(Math.random()*4)];
-	console.log(newColor);
+	sequence.push(newColor);
+	//console.log(sequence);
 }
 
-function userColor() {
-		//send clicked color to testSequence[]
+function userColor(color) {
+	//send clicked color to testSequence[]
+	testSequence.push(color);
+	console.log(testSequence);
+	
 }
 
 
 // event listeners
 document.getElementById("blue").addEventListener("click", function() {
-		
+	userColor('blue');
 });
 
 document.getElementById("red").addEventListener("click", function() {
-		
+	userColor('red');
 });
 
 document.getElementById("yellow").addEventListener("click", function() {
-		
+	userColor('yellow');
 });
 
 document.getElementById("green").addEventListener("click", function() {
-		
+	userColor('green');
 });
+
+
 /*
  * use arrays to store game sequence and user inputs
  * make code readable via many functions
