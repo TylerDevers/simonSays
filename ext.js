81bb6d76f0554a93fdd853705e9c22ed32cc221a
TylerDevers <devers.tyler@gmail.com>
2017-09-15 11:33:42 -0700

    stored last object centered code in tested.js and started over using only functions

--- ext.js
+++ ext.js
@@ -1,56 +1,45 @@
- 
-var yellow = "yellow";
-var red = "red", blue = "blue", yellow = "yellow", green = "green";
-var lightYellow = "#FBFB98", lightRed = "#FB9595", lightBlue = "#98CCFF",
+var red = "red", blue = "blue", yellow = "yellow", green = "green", 
+		lightYellow = "#FBFB98", lightRed = "#FB9595", lightBlue = "#98CCFF",
 		lightGreen = "#84FA84";
+var colors = [red, blue, yellow, green], sequence = [], testSequence = [];
 var index = 0;
+/*
+ * the Broad strokes:
+ * game sequences will be stored in arrays
+ */
+function nextColor() {
+	var newColor = colors[Math.floor(Math.random()*4)];
+	console.log(newColor);
+}
 
-var game = {
-	gamePlay: function() {
-		//starts new  game
-			if (game.colorSeq == 0) {
-					game.round();
-			} else {
-					if (game.colorSeq[index] == game.seqTest[index]) {
-						console.log("correct ");
-						
-					}
-			}
-	},
-	colorSeq: [], // store  game colors
-	seqTest: [], // store users picks
-	round: function() { // chooses random color
-		var newColor = [yellow, red, blue, green][Math.floor(Math.random()*4)];
-		index ++;
-		game.colorSeq.push(newColor);
-		console.log(game.colorSeq);
-	}
-} 
-
-//game logic
+function userColor() {
+		//send clicked color to testSequence[]
+}
 
 
 // event listeners
 document.getElementById("blue").addEventListener("click", function() {
-		game.seqTest.push('blue');
-		game.gamePlay();
+		
 });
 
 document.getElementById("red").addEventListener("click", function() {
-		game.seqTest.push('red');
-		game.gamePlay();
+		
 });
 
 document.getElementById("yellow").addEventListener("click", function() {
-		game.seqTest.push('yellow');
-		game.gamePlay();
+		
 });
 
 document.getElementById("green").addEventListener("click", function() {
-		game.seqTest.push('green');
-		game.gamePlay();
+		
 });
 /*
+ * use arrays to store game sequence and user inputs
+ * make code readable via many functions
+ * still use event listeners.
+ * 
+ * 
+ * 
  * working on gameplay. gamelogic is iffy.
  * ideas:
  * javascript event listener that will listen for clicks on the div tiles
